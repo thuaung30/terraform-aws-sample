212f1ded2466e6b2bde12a06cbeffdde4561dcc0
Thu Htet Aung <thuhtetaung.d2@gmail.com>
2023-03-17 11:43:48 +0630

    Changed allow all ingress in pri nacl

--- network_acls.tf
+++ network_acls.tf
@@ -82,61 +82,61 @@ resource "aws_network_acl" "foo_pri_nacl" {
   vpc_id     = aws_vpc.foo_vpc.id
   subnet_ids = [aws_subnet.foo_pri_sub.id]
 
-  ingress {
-    protocol   = "tcp"
-    rule_no    = 100
-    action     = "allow"
-    cidr_block = aws_vpc.foo_vpc.cidr_block
-    from_port  = 22
-    to_port    = 22
-  }
-
-  ingress {
-    protocol   = "tcp"
-    rule_no    = 200
-    action     = "allow"
-    cidr_block = aws_vpc.foo_vpc.cidr_block
-    from_port  = 23
-    to_port    = 23
-  }
-
-  ingress {
-    protocol   = "tcp"
-    rule_no    = 300
-    action     = "allow"
-    cidr_block = aws_vpc.foo_vpc.cidr_block
-    from_port  = 5432
-    to_port    = 5432
-  }
-
-  ingress {
-    protocol   = "tcp"
-    rule_no    = 400
-    action     = "allow"
-    cidr_block = aws_vpc.foo_vpc.cidr_block
-    from_port  = 6379
-    to_port    = 6379
-  }
-
-  ingress {
-    protocol   = "icmp"
-    rule_no    = 500
-    action     = "allow"
-    cidr_block = aws_vpc.foo_vpc.cidr_block
-    from_port  = 0
-    to_port    = 40
-    icmp_type  = -1
-    icmp_code  = -1
-  }
-
   /* ingress { */
-  /*   protocol   = "all" */
+  /*   protocol   = "tcp" */
   /*   rule_no    = 100 */
   /*   action     = "allow" */
   /*   cidr_block = aws_vpc.foo_vpc.cidr_block */
-  /*   from_port  = 0 */
-  /*   to_port    = 0 */
+  /*   from_port  = 22 */
+  /*   to_port    = 22 */
   /* } */
+
+  /* ingress { */
+  /*   protocol   = "tcp" */
+  /*   rule_no    = 200 */
+  /*   action     = "allow" */
+  /*   cidr_block = aws_vpc.foo_vpc.cidr_block */
+  /*   from_port  = 23 */
+  /*   to_port    = 23 */
+  /* } */
+
+  /* ingress { */
+  /*   protocol   = "tcp" */
+  /*   rule_no    = 300 */
+  /*   action     = "allow" */
+  /*   cidr_block = aws_vpc.foo_vpc.cidr_block */
+  /*   from_port  = 5432 */
+  /*   to_port    = 5432 */
+  /* } */
+
+  /* ingress { */
+  /*   protocol   = "tcp" */
+  /*   rule_no    = 400 */
+  /*   action     = "allow" */
+  /*   cidr_block = aws_vpc.foo_vpc.cidr_block */
+  /*   from_port  = 6379 */
+  /*   to_port    = 6379 */
+  /* } */
+
+  /* ingress { */
+  /*   protocol   = "icmp" */
+  /*   rule_no    = 500 */
+  /*   action     = "allow" */
+  /*   cidr_block = aws_vpc.foo_vpc.cidr_block */
+  /*   from_port  = 0 */
+  /*   to_port    = 40 */
+  /*   icmp_type  = -1 */
+  /*   icmp_code  = -1 */
+  /* } */
+
+  ingress {
+    protocol   = "all"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = aws_vpc.foo_vpc.cidr_block
+    from_port  = 0
+    to_port    = 0
+  }
 
   egress {
     protocol   = "all"
@@ -149,7 +149,7 @@ resource "aws_network_acl" "foo_pri_nacl" {
 
   depends_on = [
     aws_vpc.foo_vpc,
-    aws_subnet.foo_pub_sub,
+    aws_subnet.foo_pri_sub,
   ]
 
   tags = {
