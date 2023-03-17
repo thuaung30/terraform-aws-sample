85948c44e84789b0cd17fa435dc9fa2bda5588f6
Thu Htet Aung <thuhtetaung.d2@gmail.com>
2023-03-17 11:14:22 +0630

    Updated nacl

--- network_acls.tf
+++ network_acls.tf
@@ -2,41 +2,41 @@ resource "aws_network_acl" "foo_pub_nacl" {
   vpc_id     = aws_vpc.foo_vpc.id
   subnet_ids = [aws_subnet.foo_pub_sub.id]
 
-  /* ingress { */
-  /*   protocol   = "tcp" */
-  /*   rule_no    = 100 */
-  /*   action     = "allow" */
-  /*   cidr_block = "0.0.0.0/0" */
-  /*   from_port  = 22 */
-  /*   to_port    = 22 */
-  /* } */
-
-  /* ingress { */
-  /*   protocol   = "tcp" */
-  /*   rule_no    = 200 */
-  /*   action     = "allow" */
-  /*   cidr_block = "0.0.0.0/0" */
-  /*   from_port  = 443 */
-  /*   to_port    = 443 */
-  /* } */
-
-  /* ingress { */
-  /*   protocol   = "tcp" */
-  /*   rule_no    = 300 */
-  /*   action     = "allow" */
-  /*   cidr_block = "0.0.0.0/0" */
-  /*   from_port  = 80 */
-  /*   to_port    = 80 */
-  /* } */
-
   ingress {
-    protocol   = "all"
+    protocol   = "tcp"
     rule_no    = 100
     action     = "allow"
     cidr_block = "0.0.0.0/0"
-    from_port  = 0
-    to_port    = 0
+    from_port  = 22
+    to_port    = 22
   }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 200
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 443
+    to_port    = 443
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 300
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 80
+    to_port    = 80
+  }
+
+  /* ingress { */
+  /*   protocol   = "all" */
+  /*   rule_no    = 100 */
+  /*   action     = "allow" */
+  /*   cidr_block = "0.0.0.0/0" */
+  /*   from_port  = 0 */
+  /*   to_port    = 0 */
+  /* } */
 
 
   egress {
@@ -62,59 +62,59 @@ resource "aws_network_acl" "foo_pri_nacl" {
   vpc_id     = aws_vpc.foo_vpc.id
   subnet_ids = [aws_subnet.foo_pri_sub.id]
 
-  /* ingress { */
-  /*   protocol   = "tcp" */
-  /*   rule_no    = 100 */
-  /*   action     = "allow" */
-  /*   cidr_block = aws_vpc.foo_vpc.cidr_block */
-  /*   from_port  = 22 */
-  /*   to_port    = 22 */
-  /* } */
-
-  /* ingress { */
-  /*   protocol   = "tcp" */
-  /*   rule_no    = 200 */
-  /*   action     = "allow" */
-  /*   cidr_block = aws_vpc.foo_vpc.cidr_block */
-  /*   from_port  = 23 */
-  /*   to_port    = 23 */
-  /* } */
-
-  /* ingress { */
-  /*   protocol   = "tcp" */
-  /*   rule_no    = 300 */
-  /*   action     = "allow" */
-  /*   cidr_block = aws_vpc.foo_vpc.cidr_block */
-  /*   from_port  = 5432 */
-  /*   to_port    = 5432 */
-  /* } */
-
-  /* ingress { */
-  /*   protocol   = "tcp" */
-  /*   rule_no    = 400 */
-  /*   action     = "allow" */
-  /*   cidr_block = aws_vpc.foo_vpc.cidr_block */
-  /*   from_port  = 6379 */
-  /*   to_port    = 6379 */
-  /* } */
-
-  /* ingress { */
-  /*   protocol   = "icmp" */
-  /*   rule_no    = 500 */
-  /*   action     = "allow" */
-  /*   cidr_block = aws_vpc.foo_vpc.cidr_block */
-  /*   from_port  = 0 */
-  /*   to_port    = 40 */
-  /* } */
-
   ingress {
-    protocol   = "all"
+    protocol   = "tcp"
     rule_no    = 100
     action     = "allow"
     cidr_block = aws_vpc.foo_vpc.cidr_block
-    from_port  = 0
-    to_port    = 0
+    from_port  = 22
+    to_port    = 22
   }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 200
+    action     = "allow"
+    cidr_block = aws_vpc.foo_vpc.cidr_block
+    from_port  = 23
+    to_port    = 23
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 300
+    action     = "allow"
+    cidr_block = aws_vpc.foo_vpc.cidr_block
+    from_port  = 5432
+    to_port    = 5432
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 400
+    action     = "allow"
+    cidr_block = aws_vpc.foo_vpc.cidr_block
+    from_port  = 6379
+    to_port    = 6379
+  }
+
+  ingress {
+    protocol   = "icmp"
+    rule_no    = 500
+    action     = "allow"
+    cidr_block = aws_vpc.foo_vpc.cidr_block
+    from_port  = 0
+    to_port    = 40
+  }
+
+  /* ingress { */
+  /*   protocol   = "all" */
+  /*   rule_no    = 100 */
+  /*   action     = "allow" */
+  /*   cidr_block = aws_vpc.foo_vpc.cidr_block */
+  /*   from_port  = 0 */
+  /*   to_port    = 0 */
+  /* } */
 
   egress {
     protocol   = "all"
